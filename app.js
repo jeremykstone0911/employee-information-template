1fe08f30fbd0e6b28f137969cb9bf5f7c3e8526d
jeremykstone0911 <61753461+jeremykstone0911@users.noreply.github.com>
2020-08-14 22:34:59 -0500

    not rendering correctly

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const Manager = require("./lib/Manager");
 const Engineer = require("./lib/Engineer");
 const Intern = require("./lib/Intern");
+// const Employee = require("./lib/Employee.js");
+
 const inquirer = require("inquirer");
 const path = require("path");
 const fs = require("fs");
@@ -10,26 +12,176 @@ const outputPath = path.join(OUTPUT_DIR, "team.html");
 
 const render = require("./lib/htmlRenderer");
 
+const employeesArray = [];
 
-// Write code to use inquirer to gather information about the development team members,
-// and to create objects for each team member (using the correct classes as blueprints!)
+function managerQuestions() {
+  return inquirer
+    .prompt([
+      {
+        type: "input",
+        name: "managersName",
+        message: "What is your manager's name?",
+      },
+      {
+        type: "input",
+        name: "managersId",
+        message: "What is your manager's ID number?",
+      },
+      {
+        type: "input",
+        name: "managersEmail",
+        message: "What is your manager's email address?",
+      },
+      {
+        type: "input",
+        name: "managersOfficePhone",
+        message: "What is your manager's office phone number?",
+      },
+    ])
+    .then(function (answers) {
+      const manager = new Manager(
+        answers.managersName,
+        answers.managersId,
+        answers.managersEmail,
+        answers.managersOfficePhone
+      );
+      employeesArray.push(manager);
+      addEmployee();
+    });
+}
 
-// After the user has input all employees desired, call the `render` function (required
-// above) and pass in an array containing all employee objects; the `render` function will
-// generate and return a block of HTML including templated divs for each employee!
+function engineerQuestions() {
+  return inquirer
+    .prompt([
+      {
+        type: "input",
+        name: "engineersName",
+        message: "What is your engineer's name?",
+      },
+      {
+        type: "input",
+        name: "engineersId",
+        message: "What is your engineer's ID number?",
+      },
+      {
+        type: "input",
+        name: "engineersEmail",
+        message: "What is your engineer's email address?",
+      },
+      {
+        type: "input",
+        name: "engineersGitHub",
+        message: "What is your engineer's GitHub username?",
+      },
+    ])
+    .then(function (answers) {
+      const engineer = new engineer(
+        answers.engineersName,
+        answers.engineersId,
+        answers.engineersEmail,
+        answers.engineersGitHub
+      );
+      employeesArray.push(engineer);
+      addEmployee();
+    });
+}
 
-// After you have your html, you're now ready to create an HTML file using the HTML
-// returned from the `render` function. Now write it to a file named `team.html` in the
-// `output` folder. You can use the variable `outputPath` above target this location.
-// Hint: you may need to check if the `output` folder exists and create it if it
-// does not.
+function internQuestions() {
+  return inquirer
+    .prompt([
+      {
+        type: "input",
+        name: "internsName",
+        message: "What is your intern's name?",
+      },
+      {
+        type: "input",
+        name: "internsId",
+        message: "What is your intern's ID number?",
+      },
+      {
+        type: "input",
+        name: "internsEmail",
+        message: "What is your intern's email address?",
+      },
+      {
+        type: "input",
+        name: "internsSchool",
+        message: "What is your intern's school?",
+      },
+    ])
+    .then((answers) => {
+      const intern = new intern(
+        answers.internsName,
+        answers.internsId,
+        answers.internsEmail,
+        answers.internsSchool
+      );
+      employeesArray.push(intern);
+      addEmployee();
+    });
+}
 
-// HINT: each employee type (manager, engineer, or intern) has slightly different
-// information; write your code to ask different questions via inquirer depending on
-// employee type.
+async function addEmployee() {
+  return await inquirer
+    .prompt([
+      {
+        type: "list",
+        name: "role",
+        message: "What position are you adding?",
+        choices: ["Manager", "Engineer", "Intern", "None"],
+      },
+    ])
+    .then(function (answers) {
+      switch (answers.role) {
+        case "Manager":
+          managerQuestions();
+          break;
+        case "Engineer":
+          engineerQuestions();
+          break;
+        case "Intern":
+          internQuestions();
+          break;
+        default:
+          buildTeam();
+      }
+    });
+}
+// function addEmployee() {
+//   inquirer
+//     .prompt([
+//       {
+//         type: "list",
+//         name: "memberChoice",
+//         message: "Which type of team member would you like to add?",
+//         choices: [
+//           "Manager",
+//           "Engineer",
+//           "Intern",
+//           "I don't want to add any more team members",
+//         ],
+//       },
+//     ])
+//     .then((userChoice) => {
+//       switch (userChoice.memberChoice) {
+//         case "Engineer":
+//           addEngineer();
+//           break;
+//         case "Intern":
+//           addIntern();
+//           break;
+//         default:
+//           buildTeam();
+//       }
+//     });
+// }
 
-// HINT: make sure to build out your classes first! Remember that your Manager, Engineer,
-// and Intern classes should all extend from a class named Employee; see the directions
-// for further information. Be sure to test out each class and verify it generates an
-// object with the correct structure and methods. This structure will be crucial in order
-// for the provided `render` function to work! ```
+function buildTeam() {
+  if (!fs.existsSync(OUTPUT_DIR)) {
+    fs.mkdirSync(OUTPUT_DIR);
+  }
+  fs.writeFileSync(outputPath, render(employeesArray), "utf-8");
+}
+
+addEmployee();
